a1b955c1ff2da2cf1596eea64435128d676851c1
Mukesh K <me.mukesh18@gmail.com>
2021-05-13 21:34:15 +0530

    Created custome Picker

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppPicker from './app/component/AppPicker';
 
 import AppScreen from './app/component/AppScreen';
 import AppTextInput from './app/component/AppTextInput';
 
+const categories = [
+  {label: "Furniture", value: 1},
+  {label: "Clothing", value: 2},
+  {label: "Cameras", value: 3},
+  
+]
 
 
 export default function App() {
-  
+  const [category, setCategory] = useState();
+
   return (
     <AppScreen>
-      <AppTextInput placeholder="User Name" icon="email"/>
+      <AppPicker selectedItem={category} onSelectItem={item => setCategory(item)} icon="apps" items={categories} placeholder="Category" />
+      <AppTextInput icon="email" placeholder="Email" />
     </AppScreen>
   ); 
 }
