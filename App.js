7befafc5a141f8bfee0d5c91443392c5626e89ce
Mukesh K <me.mukesh18@gmail.com>
2021-05-13 06:24:24 +0530

    Added Message Screen & Related Componenets

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessageScreen from './app/screens/MessageScreen';
 
 export default function App() {
-  return <ListingDetailsScreen />
+  return <MessageScreen />
 }
