d7b36d20c1742869214f45bc1d8a32bdde8adfa8
Mukesh K <me.mukesh18@gmail.com>
2021-05-16 11:27:07 +0530

    Added ImageInput component using expo image picker

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import React, { useState } from 'react';
+
+import AppScreen from './app/component/AppScreen';
+import ImageInput from './app/component/ImageInput';
+
 
 export default function App() {
-  
-  return <ListingEditScreen />
+  const [imageUri, setImageUri] = useState();
+  return(
+    <AppScreen>
+      <ImageInput imageUri={imageUri} onChangeImage={uri => setImageUri(uri)}/>
+    </AppScreen>
+  );
 }
